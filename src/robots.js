c80a406aff2314e84051eecfb9e04d473f10ecad
shera19 <aybekov.shera@gmail.com>
2020-11-26 20:55:43 -0800

    Robofriends as a contact list created and finished, but add some features in future

---
+++ src/robots.js
@@ -0,0 +1,65 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jamaldin Vaninov',
+        username: 'Jamik',
+        email: 'jamik@gmail.com'
+
+    },
+    {
+        id: 2,
+        name: 'Koshoi Arstanbekov',
+        username: 'Kesha',
+        email: 'koshoi@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Majit Smazov',
+        username: 'Macho',
+        email: 'macho@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Asmanay Paiazova',
+        username: 'Asmanay',
+        email: 'asmanay@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Emil Imanov',
+        username: 'Cherniy',
+        email: 'cherniy@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Rauf Shimarov',
+        username: 'Raufchik',
+        email: 'rauf@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Aidar Jantaev',
+        username: 'Aidar',
+        email: 'aidar@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Bekzat Imanaliev',
+        username: 'Beka',
+        email: 'beka@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Sultan Isakov',
+        username: 'Sula',
+        email: 'sula@gmail.com'
+
+    },
+    {
+        id: 10,
+        name: 'Nurtay ',
+        username: 'Nuraty',
+        email: 'Nurtay@gmail.com'
+    },
+
+]
